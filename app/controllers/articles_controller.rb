0756a2cb62e0ce452bf118d52d4a54a4780c7da2
Usama <usamaikram82@gmail.com>
2020-08-21 20:06:33 +0500

    Added Styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
+        flash[:alert] = "Article was deleted successfully"
         redirect_to articles_path
     end
 
