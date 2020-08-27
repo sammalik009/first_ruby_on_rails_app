034b83c494437e36c73a0092b74ceaa8235a8a6f
Usama <usamaikram82@gmail.com>
2020-08-27 20:54:31 +0500

    Some new changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 125 }, uniqueness: { case_sensitive: false}, format: { with: VALID_EMAIL_REGEX}
     has_many :articles
 end
