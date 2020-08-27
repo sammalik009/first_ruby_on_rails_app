5e091b90a60bd8f2c29ecae60e45f109fa6d2c83
Usama <usamaikram82@gmail.com>
2020-08-27 16:31:09 +0500

    user model created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 6, maximum: 25 }, uniqueness: true
     validates :description, presence: true, length: { minimum: 6, maximum: 100 }
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_091925) do
+ActiveRecord::Schema.define(version: 2020_08_27_111501) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.text "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
